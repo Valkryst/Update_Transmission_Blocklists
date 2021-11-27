a8382f51c0d2df45fb485734ef7a4384cc5abb81
Valkryst <valkryst@valkryst.com>
2021-11-27 15:02:38 -0400

    Fix issue where settings were updated while `transmission-daemon` was running
    Fix issue where the incorrect settings file was being updated

--- update.sh
+++ update.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+# Stop Transmission
+service transmission-daemon stop
+
+
+
 # Check if Transmission is Installed
 if [[ -z $(type transmission-daemon 2>&1) ]]; then
-	echo "The 'transmission-daemon' command could not be found, exiting."
-	exit
+        echo "The 'transmission-daemon' command could not be found, exiting."
+        exit
 fi
 
 
 
 # Enable Blocklists in Transmission
-sed -i 's/"blocklist-enabled": false/"blocklist-enabled": true/g' "/etc/transmission-daemon/settings.json"
-
+sed -i 's/"blocklist-enabled": false/"blocklist-enabled": true/g' "/var/lib/transmission-daemon/info/settings.json"
 
 
 # Create & CD Into Blocklists Folder
 if [[ ! -d "/var/lib/transmission-daemon/info/blocklists" ]]; then
-	mkdir "/var/lib/transmission-daemon/info/blocklists"
+        mkdir "/var/lib/transmission-daemon/info/blocklists"
 fi
 
 cd "/var/lib/transmission-daemon/info/blocklists"
@@ -24,40 +28,40 @@ cd "/var/lib/transmission-daemon/info/blocklists"
 
 # Download Blocklists
 blocklists=(
-	"https://raw.githubusercontent.com/Naunter/BT_BlockLists/master/bt_blocklists.gz"
+        "https://raw.githubusercontent.com/Naunter/BT_BlockLists/master/bt_blocklists.gz"
 )
 
 for url in "${blocklists[@]}"
 do
-	wget $url
+        wget $url
 done
 
 
 
 # Extract Common Archives
 if [[ -n $(type unzip 2>&1) ]]; then
-	ls | grep ".*\.zip$" | xargs unzip -o >/dev/null 2>/dev/null
+        ls | grep ".*\.zip$" | xargs unzip -o >/dev/null 2>/dev/null
 fi
 
 if [[ -n $(type tar 2>&1) ]]; then
-	ls | grep ".*\.tar.gz$" | xargs tar -x -z -f >/dev/null 2>/dev/null
+        ls | grep ".*\.tar.gz$" | xargs tar -x -z -f >/dev/null 2>/dev/null
 fi
 
 if [[ -n $(type gzip 2>&1) ]]; then
-	ls | grep ".*\.gz$" | xargs gzip -d -f >/dev/null 2>/dev/null
+        ls | grep ".*\.gz$" | xargs gzip -d -f >/dev/null 2>/dev/null
 fi
 
 if [[ -n $(type p7zip 2>&1) ]]; then
-	ls | grep ".*\.7z$" | xargs p7zip -d -f >/dev/null 2>/dev/null
+        ls | grep ".*\.7z$" | xargs p7zip -d -f >/dev/null 2>/dev/null
 fi
 
 
 
-# Restart Transmission
-sudo /etc/init.d/transmission-daemon reload
+# Start Transmission
+service transmission-daemon restart
 
 
 
 # Delete Downloaded Files
-sleep 1m
+sleep 15s
 ls | grep -v ".*\.bin" | xargs rm
